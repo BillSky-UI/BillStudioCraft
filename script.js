ef658abd1dbf56bd937a6b33cdd2de44d0d68df3
BillSky-UI <nabilalkaysan0@gmail.com>
2024-09-05 21:58:11 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-var hamburger = document.querySelector(".hamb");
-var navlist = document.querySelector(".nav-list");
-var links = document.querySelector(".nav-list li");
-
-hamburger.addEventListener("click", function(){
-    this.classList.toggle("click");
-    navlist.classList.toggle("open");
-});
